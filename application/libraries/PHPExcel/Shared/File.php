3bdbc906515ced2ccbe1b4946afcd1c620ea5998
JLamim Projetos Web & Mobile <dev@jlamim.com.br>
2016-06-16 10:56:26 -0300

    Ajustes na library PHPExcel p/ funcionar com PHP7
    Corrigidos os arquivos Shared/File.php e Calculation/Functions.php

--- application/libraries/PHPExcel/Shared/File.php
+++ application/libraries/PHPExcel/Shared/File.php
@@ -172,7 +172,11 @@ class PHPExcel_Shared_File
 		// use ordinary built-in PHP function
 		//	There should be no problem with the 5.2.4 Suhosin realpath() bug, because this line should only
 		//		be called if we're running 5.2.1 or earlier
-		return realpath(sys_get_temp_dir());
+		if(version_compare(phpversion(),"5.2.4") <= 0){
+			return realpath(sys_get_temp_dir());
+		}else{
+			return sys_get_temp_dir();
+		}
 	}
 
 }
